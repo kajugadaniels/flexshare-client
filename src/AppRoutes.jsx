20c3b042b58808740897d1e3929111985b18c9cf
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-14 18:36:41 +0200

    Setup main features

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Contact, GetProductDetails, GetProducts, Home, HowItWorks, Login, Register, WhoWeAre } from './pages'
+import UserLayout from './layouts/UserLayout'
+import { Route, Routes } from 'react-router-dom'
 
 const AppRoutes = () => {
     return (
-        <div>AppRoutes</div>
+        <Routes>
+            <Route element={<UserLayout />}>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                <Route path="/" element={<Home />} />
+                <Route path="/who-we-are" element={<WhoWeAre />} />
+                <Route path="/how-it-works" element={<HowItWorks />} />
+                <Route path="/contact-us" element={<Contact />} />
+
+                <Route path="/products" element={<GetProducts />} />
+                <Route path="/product/:slug" element={<GetProductDetails />} />
+            </Route>
+        </Routes>
     )
 }
 
