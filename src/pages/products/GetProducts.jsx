68ba8a535029e6349aca9222ef089fccd6d45a20
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-15 13:16:06 +0200

    Setup projects pages

--- src/pages/products/GetProducts.jsx
+++ src/pages/products/GetProducts.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const GetProducts = () => {
     return (
-        <div>GetProducts</div>
+
+        <div className="breadcrumbs">
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-lg-6 col-md-6 col-12">
+                        <div className="breadcrumbs-content">
+                            <h1 className="page-title">Products</h1>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-12">
+                        <ul className="breadcrumb-nav">
+                            <li>
+                                <a href="/">
+                                    Home
+                                </a>
+                            </li>
+                            <li>Products</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
     )
 }
 
