dafeecd365eff5f77e4df312a5ad2812114bad6c
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-17 22:58:14 +0200

    Redirect link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,21 @@ const Footer = () => {
                             <div className="single-footer mobile-app">
                                 <h3>Mobile Apps</h3>
                                 <div className="app-button">
-                                    <a href="javascript:void(0)" className="btn">
+                                    <a href="#" className="btn">
                                         <i className="lni lni-play-store"></i>
                                         <span className="text">
-                                            <span className="small-text">Get It On</span>
+                                            <span className="small-text">
+                                                Get It On
+                                            </span>
                                             Google Play
                                         </span>
                                     </a>
-                                    <a href="javascript:void(0)" className="btn">
+                                    <a href="#" className="btn">
                                         <i className="lni lni-apple"></i>
                                         <span className="text">
-                                            <span className="small-text">Get It On</span>
+                                            <span className="small-text">
+                                                Get It On
+                                            </span>
                                             App Store
                                         </span>
                                     </a>
@@ -31,21 +35,13 @@ const Footer = () => {
                             <div className="single-footer f-link">
                                 <h3>Locations</h3>
                                 <div className="row">
-                                    <div className="col-lg-6 col-md-6 col-12">
+                                    <div className="col-lg-12 col-md-12 col-12">
                                         <ul>
-                                            <li><a href="javascript:void(0)">Chicago</a></li>
-                                            <li><a href="javascript:void(0)">New York City</a></li>
-                                            <li><a href="javascript:void(0)">San Francisco</a></li>
-                                            <li><a href="javascript:void(0)">Washington</a></li>
-                                            <li><a href="javascript:void(0)">Boston</a></li>
-                                        </ul>
-                                    </div>
-                                    <div className="col-lg-6 col-md-6 col-12">
-                                        <ul>
-                                            <li><a href="javascript:void(0)">Los Angeles</a></li>
-                                            <li><a href="javascript:void(0)">Seattle</a></li>
-                                            <li><a href="javascript:void(0)">Las Vegas</a></li>
-                                            <li><a href="javascript:void(0)">San Diego</a></li>
+                                            <li><a href="#">Kigali City</a></li>
+                                            <li><a href="#">Northern Province</a></li>
+                                            <li><a href="#">Southern Province</a></li>
+                                            <li><a href="#">Eastern Province</a></li>
+                                            <li><a href="#">Western Province</a></li>
                                         </ul>
                                     </div>
                                 </div>
@@ -55,11 +51,11 @@ const Footer = () => {
                             <div className="single-footer f-link">
                                 <h3>Quick Links</h3>
                                 <ul>
-                                    <li><a href="javascript:void(0)">About Us</a></li>
-                                    <li><a href="javascript:void(0)">How It's Works</a></li>
-                                    <li><a href="javascript:void(0)">Login</a></li>
-                                    <li><a href="javascript:void(0)">Signup</a></li>
-                                    <li><a href="javascript:void(0)">Help & Support</a></li>
+                                    <li><a href="/who-we-are">About Us</a></li>
+                                    <li><a href="/how-it-works">How It's Works</a></li>
+                                    <li><a href="/login">Login</a></li>
+                                    <li><a href="/register">Signup</a></li>
+                                    <li><a href="/help-support">Help & Support</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -67,8 +63,14 @@ const Footer = () => {
                             <div className="single-footer f-contact">
                                 <h3>Contact</h3>
                                 <ul>
-                                    <li>23 New Design Str, Lorem Upsum 10<br /> Hudson Yards, USA</li>
-                                    <li>Tel. +(123) 1800-567-8990 <br /> Mail. support@classigrids.com</li>
+                                    <li>
+                                        Kigali Rwanda
+                                    </li>
+                                    <li>
+                                        +250 788 888 888
+                                        <br />
+                                        info@flexshare.rw
+                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -82,20 +84,20 @@ const Footer = () => {
                             <div className="col-12">
                                 <div className="content">
                                     <ul className="footer-bottom-links">
-                                        <li><a href="javascript:void(0)">Terms of use</a></li>
-                                        <li><a href="javascript:void(0)"> Privacy Policy</a></li>
-                                        <li><a href="javascript:void(0)">Advanced Search</a></li>
-                                        <li><a href="javascript:void(0)">Site Map</a></li>
-                                        <li><a href="javascript:void(0)">Information</a></li>
+                                        <li><a href="#">Terms of use</a></li>
+                                        <li><a href="#"> Privacy Policy</a></li>
+                                        <li><a href="#">Advanced Search</a></li>
+                                        <li><a href="#">Site Map</a></li>
+                                        <li><a href="#">Information</a></li>
                                     </ul>
-                                    <p className="copyright-text">Designed and Developed by <a href="https://graygrids.com/"
-                                        rel="nofollow" target="_blank">GrayGrids</a>
+                                    <p className="copyright-text">
+                                        Developed by NEXCODE
                                     </p>
                                     <ul className="footer-social">
-                                        <li><a href="javascript:void(0)"><i className="lni lni-facebook-filled"></i></a></li>
-                                        <li><a href="javascript:void(0)"><i className="lni lni-twitter-original"></i></a></li>
-                                        <li><a href="javascript:void(0)"><i className="lni lni-youtube"></i></a></li>
-                                        <li><a href="javascript:void(0)"><i className="lni lni-linkedin-original"></i></a></li>
+                                        <li><a href="#"><i className="lni lni-facebook-filled"></i></a></li>
+                                        <li><a href="#"><i className="lni lni-twitter-original"></i></a></li>
+                                        <li><a href="#"><i className="lni lni-youtube"></i></a></li>
+                                        <li><a href="#"><i className="lni lni-linkedin-original"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
