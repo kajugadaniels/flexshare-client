9e2d812ca2875be1e789eead534ea615c1d49c99
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-18 21:09:06 +0200

    Design profile sidebar

--- src/components/ProfileSidebar.jsx
+++ src/components/ProfileSidebar.jsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 const ProfileSidebar = () => {
     return (
-        <div>ProfileSidebar</div>
+        <div class="dashboard-sidebar">
+                    <div class="user-image">
+                        <img src="https://demo.graygrids.com/themes/classigrids/assets/images/dashboard/user-image.jpg" alt="#" />
+                        <h3>
+                            Jannet Doe
+                            <span><a href="javascript:void(0)">jannet@doe.com</a></span>
+                        </h3>
+                    </div>
+                    <div class="dashboard-menu">
+                        <ul>
+                            <li>
+                                <a class="active" href="/profile">
+                                    <i class="lni lni-dashboard"></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/profile">
+                                    <i class="lni lni-pencil-alt"></i> 
+                                    Edit Profile
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/items">
+                                    <i class="lni lni-bolt-alt"></i> 
+                                    My Items
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/favorite-items">
+                                    <i class="lni lni-heart"></i> Favorite Items
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/invoices">
+                                    <i class="lni lni-printer"></i> 
+                                    Invoice
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="button">
+                            <a class="btn" href="javascript:void(0)">Logout</a>
+                        </div>
+                    </div>
+                </div>
     )
 }
 
