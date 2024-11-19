923977a9a069ab892e6a806245ca29a4f4b57c63
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-19 18:48:12 +0200

    Integrate GoogleOAuthProvider in App.jsx to enable Google Authentication across the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './AppRoutes';
-import './assets/css/main.css'
+import './assets/css/main.css';
+
+const GOOGLE_CLIENT_ID = '165505940635-oda6u8kfjnsgnit4gqfrvpekdd15aqo7.apps.googleusercontent.com';
 
 const App = () => {
     return (
-        <Router>
-            <AppRoutes />
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={true}
-                newestOnTop
-                closeOnClick
-                pauseOnFocusLoss={false}
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-        </Router>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <Router>
+                <AppRoutes />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={true}
+                    newestOnTop
+                    closeOnClick
+                    pauseOnFocusLoss={false}
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
+            </Router>
+        </GoogleOAuthProvider>
     );
 };
 
